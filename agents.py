7656895eb99e9da9990facf737081a0ba697570b
Abhi <johnabhi744@gmail.com>
2026-07-02 18:22:17 +0530

    due to Quota issues changed openAI to Google's Gemini API

--- agents.py
+++ agents.py
@@ -1,5 +1,5 @@
 from langchain.agents import create_agent
-from langchain_openai import ChatOpenAI
+from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from tools import web_search , scrape_url 
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-llm = ChatOpenAI(model = "gpt-4o-mini",temperature=0)
+llm = ChatGoogleGenerativeAI(model="gemini-3.5-flash", temperature=0)
 
 
 
